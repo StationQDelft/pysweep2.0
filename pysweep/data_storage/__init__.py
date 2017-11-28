d06ad19edd74ca3fa88c675b7e388c0eba31080c
Wolfgang Pfaff <wolfgangpfff@gmail.com>
2017-11-28 09:26:27 +0100

    new data storage format using numpy arrays as basis; initial version, not fully tested.

--- pysweep/data_storage/__init__.py
+++ pysweep/data_storage/__init__.py
@@ -1,3 +1,4 @@
 from pysweep.data_storage.json import JSONStorage
 from pysweep.data_storage.spyview import SpyviewStorage
+from pysweep.data_storage.nparray import NpStorage
 from pysweep.utils import sleep
